56a75a6a480cd4c29ed949292f0c4cd133800b57
Richard Corn <rac@swdevs.com>
2019-10-25 17:41:54 -0700

    Update comments.

--- src/main/java/Team4450/Robot19/SubSystem.java
+++ src/main/java/Team4450/Robot19/SubSystem.java
@@ -6,26 +6,26 @@ package Team4450.Robot19;
 
 public abstract class SubSystem
 {
-	/*
+	/**
 	 * Called when robot is enabled. Subsystem should set it's starting
 	 * configuration.
 	 */
 	abstract void enable();
 	
-	/*
+	/**
 	 * Called when robot is disabled. Subsystem should set it's stopped
 	 * configuration. Any PID loops or threads should be stopped.
 	 */
 	abstract void disable();
 	
-	/*
+	/**
 	 * Called when the subsystem instance is released. All resources that
 	 * need to be released should be released and singleton variable set
 	 * to null.
 	 */
 	abstract void dispose();
 	
-	/*
+	/**
 	 * Common function where subsystem should set the DS indicators to the
 	 * subsystem's current status as appropriate.
 	 */
